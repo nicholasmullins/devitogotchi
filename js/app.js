e03e90b48705012e444cd87e895b522fdb5359f5
Home <home@Homes-MacBook.local>
2019-10-04 14:45:13 -0600

    Got most of the bugs out of the game. Devito can die now. Go to Bed function works as well as it can

--- js/app.js
+++ js/app.js
@@ -13,14 +13,19 @@ const pet = {
 
 
 var image_tracker = 'green';
+var image_tracker2 = 'green';
 
 
 
 let energyLevel = setInterval(decreaseEnergy, 2000);
 let hungerLevel = setInterval(decreaseHunger, 2000);
 let funLevel = setInterval(decreaseFun, 2000);
-let ageLevel = setInterval(increaseAge, 20000);
+let ageLevel = setInterval(increaseAge, 5000);
 let animatedPet = setInterval(movePet, 1000); 
+let animatedMorph = setInterval(moveMorphPet, 1000);
+let deathCheck = setInterval(checkForDeath, 1000);
+
+
 
 function decreaseEnergy() {
 	pet.energy -= 5;
@@ -59,49 +64,85 @@ function movePet() {
 		$('#stickChange').html('<img id="stickMan" src="/Users/home/sei-cranberry-gobblers/devitogotchi/images/rightside.png">');
  		image_tracker='blue';
  		console.log('maybe')
-	}
-}
+	} 
 
-// movePet();
-// decreaseEnergy();
-// decreaseHunger();
-// decreaseFun();
+	
+};
+
+
+function moveMorphPet () {
+if (pet.age > 2) {
+		clearInterval(animatedPet)			
+		if(image_tracker2 =='green'){
+ 		$('#stickChange').html('<img id="stickMan" src="/Users/home/sei-cranberry-gobblers/devitogotchi/images/devitofullleft.png">');
+ 		image_tracker2='red';
+ 	} else if (image_tracker2 == 'red'){
+ 		$('#stickChange').html('<img id="stickMan" src="/Users/home/sei-cranberry-gobblers/devitogotchi/images/devitofullright.png">');
+ 		image_tracker2='green';		
+	} 
+};
+};
 
 $('#wakeUp').on('click', () => {
 	let energyLevel = setInterval(decreaseEnergy, 2000);
 	let hungerLevel = setInterval(decreaseHunger, 2000);
 	let funLevel = setInterval(decreaseFun, 2000);
 	let ageLevel = setInterval(increaseAge, 20000);
+	
+	if (pet.age < 2) {
 	let animatedPet = setInterval(movePet, 1000); 
-})
+	} else {
+    let animatedMorph = setInterval(moveMorphPet, 1000);
+	}
+	$('#column2').css('background-blend-mode', '');
+});
 
 
+function checkForDeath() {
 
 
-function goToSleep() {
-	clearInterval(animatedPet);
+if (pet.energy < 1) {
+	alert('Devito has died of exhaustion')
+	console.log('he is dead')
+	clearInterval(deathCheck);
+	clearInterval(ageLevel);
 	clearInterval(energyLevel);
 	clearInterval(funLevel);
 	clearInterval(hungerLevel);
-	$('#column2').css('background-blend-mode', 'multiply');
-	$('#stickMan').attr('src', '/Users/home/sei-cranberry-gobblers/devitogotchi/images/sleep.png');
-	pet.energy = 100;
+} else if (pet.fun < 1) {
+	alert('Devito has died of boredom')
+	clearInterval(energyLevel);
+	clearInterval(funLevel);
+	clearInterval(hungerLevel);
+	clearInterval(deathCheck);
+	clearInterval(ageLevel);
+} else if (pet.hunger < 1) {
+	alert('Devito has died of hunger')
+	clearInterval(energyLevel);
+	clearInterval(funLevel);
+	clearInterval(hungerLevel);
+	clearInterval(deathCheck);
+	clearInterval(ageLevel);
+	
 };
 
-
-
-$('#wakeUp').on('click', () => {
-	movePet();
-	decreaseEnergy();
-	decreaseHunger();
-	decreaseFun();
-})
-
-
+};
 
 $('#goBed').on('click', () => {
-	goToSleep();
+	clearInterval(energyLevel);
+	clearInterval(funLevel);
+	clearInterval(hungerLevel);
+	if (pet.age < 2) {
+		clearInterval(animatedPet); 
+	} else {
+    	clearInterval(animatedMorph);
+	};
+	$('#column2').css('background-blend-mode', 'multiply');
+	$('#stickMan').attr('src', '/Users/home/sei-cranberry-gobblers/devitogotchi/images/sleep.png');
+	pet.energy += 20;
+	$('#energyMeter').css('width', `${pet.energy}%`);
 });	
+
 
 $('#giveFood').on('click', () => {
 	if (pet.hunger < 100) {
@@ -113,7 +154,6 @@ $('#giveFood').on('click', () => {
 });	
 
 $('#letsPlay').on('click', () => {
-	$('#stickMan').attr('src', '/Users/home/sei-cranberry-gobblers/devitogotchi/images/why.png');
 	if (pet.fun < 100) {
 		pet.fun +=10;
 		$('#funMeter').css('width', `${pet.fun}%`);
@@ -130,24 +170,4 @@ $('#letsPlay').on('click', () => {
 
 
 
-
-
-
-
-
-
-
-// $('img').on('click', () => {
-// 	$('#stickMan').attr('src', '/Users/home/sei-cranberry-gobblers/devitogotchi/images/why.png');
-// 	return false;
-
-// });	
-
-
-
-
-
-
-
-// // };
 
